5e40c58519f48280903b0e3bcfd18af7db1e2817
Tugane <tugane@Tuganes-MacBook-Pro.local>
2022-04-09 13:18:25 +0200

    middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import logger from "./middleware/logger";
+import toast from "./middleware/toast";
 import reducer from "./reducer";
 
 export default function () {
-  return configureStore({ reducer });
+  return configureStore({
+    reducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware()
+        .concat(logger({ destination: "console" }))
+        .concat(toast),
+  });
 }
